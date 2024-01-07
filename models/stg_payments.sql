b3364cacf3e9825bdfec4451fcefd1c067fef8c9
andres.cerv.7@gmail.com <andres.cerv.7@gmail.com>
2024-01-07 00:41:05 +0000

    renamed cols in payments model

--- models/stg_payments.sql
+++ models/stg_payments.sql
@@ -3,9 +3,9 @@ with
 payments as (
 
     select 
-        id,
-        orderid,
-        paymentmethod,
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
         status,
         amount,
         created
