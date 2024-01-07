5c23ffcbce8a1db925082e5b977a84b0f03503be
andres.cerv.7@gmail.com <andres.cerv.7@gmail.com>
2024-01-07 00:38:48 +0000

    completed CTEs

--- models/customer_orders.sql
+++ models/customer_orders.sql
@@ -8,7 +8,15 @@ with
         from {{ ref('stg_orders') }}
     ),
 
-    p as (
+    customers as (
+        select 
+            customer_id,
+            first_name,
+            last_name,
+        from {{ ref('stg_customers') }}
+    ),
+
+    payments as (
         select
             orderid as order_id,
             max(created) as payment_finalized_date,
@@ -24,13 +32,13 @@ with
             orders.customer_id,
             orders.order_date as order_placed_at,
             orders.status as order_status,
-            p.total_amount_paid,
-            p.payment_finalized_date,
-            c.first_name as customer_first_name,
-            c.last_name as customer_last_name
+            payments.total_amount_paid,
+            payments.payment_finalized_date,
+            customers.first_name as customer_first_name,
+            customers.last_name as customer_last_name
         from orders
-        left join p on orders.order_id = p.order_id
-        left join {{ ref("stg_customers") }} c on orders.customer_id = c.customer_id
+        left join payments on orders.order_id = payments.order_id
+        left join customers on orders.customer_id = customers.customer_id
     ),
 
     x as (
@@ -38,20 +46,19 @@ with
             p.order_id,
             sum(t2.total_amount_paid) as clv_bad
         from paid_orders p
-        left join paid_orders t2
-            on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
         group by 1
         order by p.order_id
     ),
 
     customer_orders as (
         select
-            c.customer_id as customer_id,
+            customers.customer_id as customer_id,
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
             count(orders.order_id) as number_of_orders
-        from {{ ref("stg_customers") }} c
-        left join {{ ref("stg_orders") }} as orders on orders.customer_id = c.customer_id
+        from customers
+        left join orders on orders.customer_id = customers.customer_id
         group by 1
     )
 
@@ -65,6 +72,6 @@ select
     c.first_order_date as fdos
 
 from paid_orders p
-    left join customer_orders as c using (customer_id)
-    left outer join x on x.order_id = p.order_id
+left join customer_orders as c using (customer_id)
+left outer join x on x.order_id = p.order_id
 order by order_id
