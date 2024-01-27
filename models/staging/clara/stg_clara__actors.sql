96f25f3a6480c06f17daf7deea8be2112cf167c8
Andres Cervantes <andres.cerv.7@gmail.com>
2024-01-27 22:22:47 +0000

    improved testing of staging models

--- models/staging/clara/stg_clara__actors.sql
+++ models/staging/clara/stg_clara__actors.sql
@@ -6,15 +6,27 @@ source as (
 
 ),
 
-transformed as (
+unique_entity as (
 
-  select distinct
+  select distinct -- deleting dups
 
     id as actor_id,
-    username as actor_username
+    username as actor_username,
 
   from source
 
+),
+
+rownumber as (
+
+    select
+
+        actor_id,
+        actor_username,
+        row_number() over(partition by actor_id order by actor_username) as rn -- a better criteria is needed to define rn
+    
+    from unique_entity
+
 )
 
-select * from transformed
+select * from rownumber
--- models/staging/clara/stg_clara__repos.sql
+++ models/staging/clara/stg_clara__repos.sql
@@ -6,15 +6,27 @@ source as (
 
 ),
 
-transformed as (
+unique_entity as (
 
-  select distinct
+  select distinct -- deleting dups
 
     id as repo_id,
     name as repo_name
-    
+
   from source
+
+),
+
+rownumber as (
+
+    select
+
+        repo_id,
+        repo_name,
+        row_number() over(partition by repo_id order by repo_name) as rn -- a better criteria is needed to define rn
+    
+    from unique_entity
 
 )
 
-select * from transformed
+select * from rownumber
